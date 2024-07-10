addd9ad49e7e41382c3f9fb2caec08826a6c4d6d
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-07-10 05:27:15 -0400

    This is initial upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('mousemove', function (e) {
+    const goodLuckSpot = document.querySelector('.good-luck-spot');
+    const rect = goodLuckSpot.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+    goodLuckSpot.style.transform = `translate(${x * 0.05}px, ${y * 0.05}px)`;
+});
