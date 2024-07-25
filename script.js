0c0ffbda2927fc206e0d861ce288056e51f366b4
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-07-25 04:42:12 -0400

    changed var to const

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var carouselElement = document.getElementById('carouselExampleDark');
+    const carouselElement = document.getElementById('carouselExampleDark');
     if (carouselElement) {
-        var carousel = new bootstrap.Carousel(carouselElement, {
-            interval: 2000,
-            wrap: true
-        });
 
         // Optional: Custom event listeners or additional functionality
         carouselElement.addEventListener('slide.bs.carousel', function (event) {
